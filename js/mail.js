a51b4a1237acaf73ea7ed81c989a0fbaedfe796b
Mateo Leiva <mateoleiva50@gmail.com>
2022-11-08 22:48:49 -0300

    agrego funcionalidad al form de contacto

---
+++ js/mail.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('button');
+const inputs = document.querySelectorAll('input');
+const isSend = document.getElementById('isSend');
+
+document.getElementById('form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.innerHTML = 'Enviando...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_478xtbj';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.innerHTML = 'Enviar';
+        inputs.forEach(input => {
+          input.value = '';
+        })
+        isSend.innerHTML = 'Mensaje Enviado'
+        setTimeout(() => {
+          isSend.innerHTML = ''
+        }, 10000);
+      }, (err) => {
+        btn.innerHTML = 'Enviar';
+      });
+  });
